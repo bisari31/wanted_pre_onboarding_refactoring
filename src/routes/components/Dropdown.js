eb4fa70bc6739a395aca49a715f90538157c9e05
bisari <bisari31@gmail.com>
2022-05-07 03:17:17 +0900

    Move: From routes folder to components folder

--- src/routes/components/Dropdown.js
+++
@@ -1,8 +0,0 @@
-import classNames from 'classnames'
-import styles from './Dropdown.module.scss'
-
-function Dropdown() {
-  return <div className={styles.aa}>1231232</div>
-}
-
-export default Dropdown
--- src/routes/components/Slider.js
+++
@@ -1,5 +0,0 @@
-function Slider() {
-  return <div>qweqw</div>
-}
-
-export default Slider
