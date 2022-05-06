c9be35ced89c7fe5e3eb9a9e5958e8f463c18b0c
bisari <bisari31@gmail.com>
2022-05-07 05:01:04 +0900

    Style: Replace with button in div

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,20 +1,21 @@
 import classNames from 'classnames/bind'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import styles from './Toggle.module.scss'
 
 const Toggle = () => {
-  const [toggle, setToggle] = useState(true)
+  const [toggle, setToggle] = useState(false)
   const cx = classNames.bind(styles)
   const handleChangeToggle = () => setToggle((prev) => !prev)
-  useEffect(() => {
-    console.log(toggle)
-  }, [toggle])
 
   return (
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div className={styles.wrapper} onClick={handleChangeToggle}>
-      <div className={cx('option', { clicked: !toggle })}>기본</div>
-      <div className={cx('option', { clicked: toggle })}>상세</div>
+      <button type='button' className={cx('option', { clicked: !toggle })}>
+        기본
+      </button>
+      <button type='button' className={cx('option', { clicked: toggle })}>
+        상세
+      </button>
       <div className={cx('toggle', { clicked: toggle })} />
     </div>
   )
