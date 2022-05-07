70c67b70b75e8f85fd7b3e0711ae75f3be1b8352
bisari <bisari31@gmail.com>
2022-05-07 20:34:37 +0900

    Fix: Add dropdown state initialization

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -37,7 +37,10 @@ const Dropdown = () => {
   }
   const handleClickOutSide = (e) => {
     console.log(categoryRef.current.contains(e.target))
-    if (viewOptions && !categoryRef.current.contains(e.target)) setViewOptions(false)
+    if (viewOptions && !categoryRef.current.contains(e.target)) {
+      setViewOptions(false)
+      setOptions([...CATEGORY])
+    }
   }
 
   useEffect(() => {
@@ -47,13 +50,13 @@ const Dropdown = () => {
     }
   })
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} ref={categoryRef}>
       <button className={styles.button} type='button' onClick={handleChagneOption}>
         {selectedOption}
         <AiFillCaretDown />
       </button>
       {viewOptions && (
-        <div className={styles.optionWrapper} ref={categoryRef}>
+        <div className={styles.optionWrapper}>
           <div className={styles.inputForm}>
             <input type='text' placeholder='Serach Symbols' className={styles.search} onChange={filterSymbols} />
             <BiSearch />
